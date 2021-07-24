36db6024a4abe91655b0274492891f13a0e4ef2e
eric zhang <ezhang323@gmail.com>
2021-07-23 19:47:04 -0700

    plssssssss

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 var crypto = require('crypto');
 
 const app = express();
-const port = 80;
+const port = 4445;
 
 app.use(express.static(__dirname + "/public"));
 
