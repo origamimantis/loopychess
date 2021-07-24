07fde7a12683dca6add72428f74a261aa1ebbae9
eric zhang <ezhang323@gmail.com>
2021-07-23 19:51:38 -0700

    sdf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 var crypto = require('crypto');
 
 const app = express();
-const port = 4445;
+const port = process.env.PORT;
 
 app.use(express.static(__dirname + "/public"));
 
