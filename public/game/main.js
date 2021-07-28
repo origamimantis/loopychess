54e6f8b99f968cc5e0f9d2ee5423bd2415ddf5c4
eric zhang <ezhang323@gmail.com>
2021-07-27 17:28:27 -0700

    remove logs

--- public/game/main.js
+++ public/game/main.js
@@ -232,7 +232,6 @@ function selectPiece(i,j,board_)
 function addIfEmpty(m, i, j, board_)
 {
   i = (i+8) % 8;
-  console.log(i);
   if (0 <= j && j < 8 && board_[j][i] === null)
   // classic
   //if (0 <= i && i < 8 && 0 <= j && j < 8 && board_[j][i] === null)
@@ -247,7 +246,6 @@ function addIfEmpty(m, i, j, board_)
 function addIfCapture(m, i, j, color, board_)
 {
   i = (i+8) % 8;
-  console.log(i);
   if (0 <= j && j < 8 && board_[j][i] !== null && board_[j][i].color !== color)
   // classic
   //if (0 <= i && i < 8 && 0 <= j && j < 8 && board_[j][i] !== null && board_[j][i].color !== color)
