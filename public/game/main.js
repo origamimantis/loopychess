3b697dbd0c857ac153391179eb8ac9169bdb57c2
eric zhang <ezhang323@gmail.com>
2021-07-27 17:26:55 -0700

    make loopy

--- public/game/main.js
+++ public/game/main.js
@@ -246,7 +246,7 @@ function addIfEmpty(m, i, j, board_)
 
 function addIfCapture(m, i, j, color, board_)
 {
-  i = i % 8;
+  i = (i+8) % 8;
   console.log(i);
   if (0 <= j && j < 8 && board_[j][i] !== null && board_[j][i].color !== color)
   // classic
